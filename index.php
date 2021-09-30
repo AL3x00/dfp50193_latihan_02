0f16e282b810d87ac5e52da867637d11deb2429a
AL3x00 <alexsaw2000@hotmail.com>
2021-09-30 10:50:25 +0800

    latihan2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+
+<body>
+    <?php
+    $name = 'Saw';
+    $umur = 20 + 1;
+
+    echo "$name berumur $umur tahun";
+    ?>
+</body>
+
+</html>
